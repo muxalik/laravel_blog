951474faa9748c0938a78355452154fae2a0f4f9
muxailk <muxail1327@mail.ru>
2022-09-13 17:58:33 +0300

    popular tags, categories refactoring

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Cache;
@@ -77,24 +78,26 @@ class AppServiceProvider extends ServiceProvider
 
             // Statistics
             // Popular tags 
-            $tags = DB::select("SELECT COUNT(*) as amount, tags.title FROM post_tag INNER JOIN tags ON tag_id = tags.id GROUP BY tag_id LIMIT 6");
+            // $tags = DB::select("SELECT COUNT(*) as amount, tags.title FROM post_tag INNER JOIN tags ON tag_id = tags.id GROUP BY tag_id LIMIT 6");
+            $tags = Tag::withCount('posts')->orderBy('posts_count', 'desc')->limit(6)->get();
             $tags_labels = $tags_posts = [];
 
             foreach ($tags as $tag) {
                 $tags_labels[] = $tag->title;
-                $tags_posts[] = $tag->amount;
+                $tags_posts[] = $tag->posts_count;
             }
 
             $view->with('tags_labels', json_encode($tags_labels));
             $view->with('tags_posts', json_encode($tags_posts));
 
             // Popular categories
-            $categories = DB::select("SELECT COUNT(*) AS amount, categories.title FROM posts INNER JOIN categories ON category_id = categories.id GROUP BY category_id");
+            // $categories = DB::select("SELECT COUNT(*) AS amount, categories.title FROM posts INNER JOIN categories ON category_id = categories.id GROUP BY category_id LIMIT 6");
+            $categories = Category::withCount('posts')->orderBy('posts_count', 'desc')->limit(6)->get();
             $categories_labels = $categories_posts = [];
 
             foreach ($categories as $category) {
                 $categories_labels[] = $category->title;
-                $categories_posts[] = $category->amount;
+                $categories_posts[] = $category->posts_count;
             }
 
             $view->with('categories_labels', json_encode($categories_labels));
