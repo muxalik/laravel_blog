068b710055c0ac08dbc360a68328247d42ed43c3
muxailk <muxail1327@mail.ru>
2022-11-10 18:43:20 +0300

    Changed actions declaration in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
+use App\Http\Controllers\Admin\MainController;
+use App\Http\Controllers\Admin\PostController as AdminPostController;
+use App\Http\Controllers\Admin\TagController as AdminTagController;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,41 +24,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PostController@index')->name('home');
-Route::get('/article/{slug}', 'PostController@show')->name('posts.single');
-Route::get('/article/{id}/loadMore', 'CommentController@loadMore')->name('comments.loadmore');
-Route::get('/category/{slug}', 'CategoryController@show')->name('categories.single');
-Route::get('/tag/{slug}', 'TagController@show')->name('tags.single');
-Route::get('/search', 'SearchController@index')->name('search');
-Route::get('/contact', 'UserController@contactForm')->name('contact');
-Route::post('/contact/store', 'UserController@contactStore')->name('contact.store');
-Route::post('/comment/{id}/store', 'CommentController@store')->name('comments.store');
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('/article/{slug}', [PostController::class, 'show'])->name('posts.single');
+Route::get('/article/{id}/loadMore', [CommentController::class, 'loadMore'])->name('comments.loadmore');
+Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('categories.single');
+Route::get('/tag/{slug}', [TagController::class, 'show'])->name('tags.single');
+Route::get('/search', [SearchController::class, 'index'])->name('search');
+Route::get('/contact', [UserController::class, 'contactForm'])->name('contact');
+Route::post('/contact/store', [UserController::class, 'contactStore'])->name('contact.store');
+Route::post('/comment/{id}/store', [CommentController::class, 'store'])->name('comments.store');
 
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'admin'], function () {
+Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
 
-    Route::get('/categories/refresh', 'CategoryController@refresh');
-    Route::get('/posts/refresh', 'PostController@refresh');
-    Route::get('/tags/refresh', 'TagController@refresh');
-    Route::get('/users/refresh', 'UserController@refresh');
+    Route::get('/categories/refresh', [AdminCategoryController::class, 'refresh']);
+    Route::get('/posts/refresh', [AdminPostController::class, 'refresh']);
+    Route::get('/tags/refresh', [AdminTagController::class, 'refresh']);
+    Route::get('/users/refresh', [AdminUserController::class, 'refresh']);
 
-    Route::get('/', 'MainController@index')->name('admin.index');
-    
+    Route::get('/', [MainController::class, 'index'])->name('admin.index');
+
     Route::resources([
-        '/categories' => 'CategoryController',
-        '/tags' => 'TagController',
-        '/posts' => 'PostController',
-        '/users' => 'UserController',
+        '/categories' => AdminCategoryController::class,
+        '/tags' => AdminTagController::class,
+        '/posts' => AdminPostController::class,
+        '/users' => AdminUserController::class,
     ]);
 });
 
 Route::group(['middleware' => 'guest'], function () {
-    Route::get('/register', 'UserController@create')->name('register.create');
-    Route::post('/register', 'UserController@store')->name('register.store');
-    Route::get('/login', 'UserController@loginForm')->name('login.create');
-    Route::post('/login', 'UserController@login')->name('login');
+    Route::get('/register', [UserController::class, 'create'])->name('register.create');
+    Route::post('/register', [UserController::class, 'store'])->name('register.store');
+    Route::get('/login', [UserController::class, 'loginForm'])->name('login.create');
+    Route::post('/login', [UserController::class, 'login'])->name('login');
 });
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/logout', 'UserController@logout')->name('logout');
+    Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 });
